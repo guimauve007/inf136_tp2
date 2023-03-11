94cd81b48fa66d8d05694d9f83b93e4b68cd2de6
liamwebersadler <74386965+liamwebersadler@users.noreply.github.com>
2023-03-11 17:03:40 -0500

    test 2

--- src/jeu.py
+++ src/jeu.py
@@ -1,5 +1,7 @@
 from constantes import JOUEUR
 from constantes import ORDINATEUR
+from constantes import HAUTEUR_GRILLE
+from constantes import LARGEUR_GRILLE
 from flotte import flotte_calculer_vie
 
 
@@ -12,16 +14,37 @@ def jeu_afficher_jouer_courant(identifiant_courant):
     elif identifiant_courant == ORDINATEUR:
         print('C\'est au tour de l\'ordinateur')
 
+
+"""
+    Saisit la position du tir du joueur et redemande jusqu'à ce que la saisit soit valide.
+
+    Arguments :
+        Aucun.
+
+    Retourne  :
+       i --> (int) : i représente la hauteur du tir saisit par l'utilisateur
+       j --> (int) : j représente la largeur du tir saisit par l'utilisateur
+    """
+
+
 def jeu_joueur_saisir_position_tir():
 
-    while
+    # Saisit de la hauteur du tir
+    i = input(int('Saisissez la hauteur de votre tir : '))
 
-    i = input(int(''))
+    #
+    while i > HAUTEUR_GRILLE and i > 1:
+        i = input(int('Saisissez la hauteur de votre tir : '))
+
+    j = input(int('Saisissez la largueur de votre tir : '))
+
+    while j > LARGEUR_GRILLE and j > 1:
+        i = input(int('Saisissez la largueur de votre tir : '))
+
+    return i, j
 
 
-    return i,j
-
-def jeu_est_partie_terminee(flotte_1,flotte_2):
+def jeu_est_partie_terminee(flotte_1, flotte_2):
     """
     Détermine si une partie est terminée. Si la vie d'une des flottes est égale à zéro, cela indique que la partie est
     complétée.
@@ -39,9 +62,9 @@ def jeu_est_partie_terminee(flotte_1,flotte_2):
     vie_flotte_2 = {flotte_calculer_vie(flotte_2)}
 
     # Comparer la vie des flottes 1 et 2 avec un booléen.
-    if  vie_flotte_1 == 0 or vie_flotte_2 == 0:
+    if vie_flotte_1 == 0 or vie_flotte_2 == 0:
 
-        # Retoruner Vrai si la partie est terminée, c'est-à-dire lorsque la vie d'une des deux flottes correspond à 0.
+        # Retourne Vrai si la partie est terminée, c'est-à-dire lorsque la vie d'une des deux flottes correspond à 0.
         return True
 
     else:
@@ -57,11 +80,11 @@ def jeu_afficher_vainqueur(flotte_joueur):
     Arguments :
         flotte_joueur [list] : correspond à la flotte du joueur
 
-    Retourne  :
+    Retourne :
         Rien.
     """
 
-    # pas sur de cette etape a voir faut il appeler lautre fonction ou faire une somme
+    # pas sur de cette etape a voir faut il appeler l'autre fonction ou faire une somme
     if flotte_joueur == 0 :
 
         # Afficher le message vainqueur de l'ordinateur contre le joueur.
