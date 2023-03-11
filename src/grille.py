4a2d84695beb901e7f2df86dbbeda7528b4fb237
Samuel <samuel.00.guy@gmail.com>
2023-03-11 16:17:17 -0500

    2 dernières fonctions dans jeu

--- src/grille.py
+++ src/grille.py
@@ -3,15 +3,15 @@ import jeu
 
 def grille_initialiser(hauteur,largeur):
 
-    #Initialiser les cases à 0
+    # Initialiser les cases à 0.
     ligne = [0] * largeur
 
-    #Initialiser le tableau à retourner
+    # Initialiser le tableau à retourner.
     grille_vierge = []
 
-    #Ajouter les lignes et les colonnes une par une
+    # Ajouter les lignes et les colonnes une par une.
     for i in range(hauteur):
         grille_vierge.append(ligne.copy())
 
-    #retourner la grille vierge
+    # Retourner la grille vierge.
     return grille_vierge
